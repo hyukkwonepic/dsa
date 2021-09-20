147142694c9c40a585b5b667e488b5e4b4f6f60e
Alex Kwon <khwalex@gmail.com>
2021-09-20 18:07:22 +0900

    Lint .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
-  extends: "eslint:recommended",
+  extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {},
 };
